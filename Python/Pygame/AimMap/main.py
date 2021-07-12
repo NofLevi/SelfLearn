563e13d3be6480043afbe5f8379b068f2c75788b
Nof <noflevi@gmail.com>
2021-07-12 13:28:45 +0300

    Main menu is ready

--- Python/Pygame/AimMap/main.py
+++ Python/Pygame/AimMap/main.py
@@ -1,5 +1,4 @@
 import main_menu_file
-import os.path
 
 def main():
     var = main_menu_file.main_menu_class()
--- Python/Pygame/AimMap/main_menu_file.py
+++ Python/Pygame/AimMap/main_menu_file.py
@@ -8,14 +8,55 @@ class main_menu_class:
     def __init__(self):
         # Setup pygame/window ---------------------------------------- #
         pygame.init()
-        DISPLAY_W, DISPLAY_H = 800 , 800
-        self.canvas = pygame.Surface((DISPLAY_W,DISPLAY_H))   
-        self.window = pygame.display.set_mode(((DISPLAY_W,DISPLAY_H)))
+        self.DISPLAY_W, self.DISPLAY_H = 400 , 400
+        self.canvas = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))   
+        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
 
         #################################################################
+        #setup sprites
+
+        #logo
+        self.canvas.fill((0,0,0))
+        my_spritesheet = sprite_sheet('logo2.jpg')
+        self.sprite_play = my_spritesheet.get_sprite(0,0,400,100)
+        self.canvas.blit(self.sprite_play, (0,0))
+        self.window.blit(self.canvas, (0,0))
+
+        #play button
         my_spritesheet = sprite_sheet('button_play.png')
-        sprite_play = my_spritesheet.get_sprite(0,0,128,128)
- 
+        self.sprite_play = my_spritesheet.get_sprite(0,0,180,60)
+        self.canvas.blit(self.sprite_play, (self.DISPLAY_W/2 - 90, self.DISPLAY_H/2 - 80))
+        self.window.blit(self.canvas, (0,0))
+        
+        #settings button
+        my_spritesheet = sprite_sheet('button_settings.png')
+        self.sprite_play = my_spritesheet.get_sprite(0,0,180,60)
+        self.canvas.blit(self.sprite_play, (self.DISPLAY_W/2 - 90, self.DISPLAY_H/2))
+        self.window.blit(self.canvas, (0,0))
+        
+
+        #quitbutton
+        my_spritesheet = sprite_sheet('button_quit.png')
+        self.sprite_play = my_spritesheet.get_sprite(0,0,180,60)
+        self.canvas.blit(self.sprite_play, (self.DISPLAY_W/2 - 90, self.DISPLAY_H/2 + 80))
+        self.window.blit(self.canvas, (0,0))
+
+        pygame.display.update()
+    
+    ##########################################################################################
+    #class functions
+    def start_game(self):
+        var = game_file.game_class()
+        var.start_game()
+    
+    def settings(self):
+        n = 1
+    
+    def quit(self):
+        self.pygame.quit()
+        sys.exit()
+
+    #pygame loop
     def start(self):
         while True:
             click = False
@@ -27,24 +68,12 @@ class main_menu_class:
                     if event.key == K_ESCAPE:
                         self.pygame.quit()
                         sys.exit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x,y = pygame.mouse.get_pos()
 
-        # loop code
-            self.canvas.fill((0,0,0))
-            self.canvas.blit(self.sprite_play, (0, self.DISPLAY_H - 128))
-            self.window.blit(self.canvas, (0,0))
-            pygame.display.update()
-            self.mainClock.tick(60)
-    
-    def start_game(self):
-        var = game_file.game_class()
-        var.start_game()
-    
-    def settings(self):
-        n = 1
-    
-    def quit(self):
-        self.pygame.quit()
-        sys.exit()
+                    if(x > 110 and x < 290 and y > 120 and y < 180):
+                        self.start_game()
+                    elif(x > 110 and x < 290 and y > 200 and y < 260):
+                        self.settings()
+                    elif(x > 110 and x < 290 and y > 280 and y < 360):
+                        self.quit()
