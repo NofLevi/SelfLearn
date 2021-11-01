0be4cc6d4ebf4a4f0a401a9f812df6db2ce13984
Nof <noflevi@gmail.com>
2021-11-01 16:04:35 +0200

    Check

--- FullStack WEB/React/Projects/nof-site/src/components/apps/Map.js
+++ FullStack WEB/React/Projects/nof-site/src/components/apps/Map.js
@@ -1,5 +1,5 @@
 import react, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 import classes from './Map.module.css';
 
 
@@ -7,11 +7,12 @@ function Map(){
     const [viewport, setViewport] = useState({
         latitude: 32.3381976,
         longitude: 34.8546063,
-        width: '100vw',
-        height: "100vh",
+        width: '99vw',
+        height: "99vh",
         zoom: 10
     
     });
+    
     // Provide your access token
 
 // Create a map in the div #map
@@ -21,7 +22,13 @@ function Map(){
         <div className = {classes.container}>
             <div className = {classes.map}> 
                 <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} mapStyle = "mapbox://styles/noflevi/ckt78ul4y25vk17ofaywuidxi" onViewportChange={(viewport) => {setViewport(viewport)}}>
-                    markers here
+                    <Marker className={classes.redmarker} latitude={22.00} longitude={86.20}>
+
+                    </Marker>
+
+                    <Marker className={classes.greenmarker} latitude={22.00} longitude={96.20}>
+
+                    </Marker>
                 </ReactMapGL>
             </div>
         </div>
