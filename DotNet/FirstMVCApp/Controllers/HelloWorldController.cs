3a0936fc2e96eb9f52fb8ddf5d9e56868097f53d
Nof <noflevi@gmail.com>
2022-03-27 16:04:05 +0300

    Learning .NET [First Project]

---
+++ DotNet/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+namespace FirstMVCApp.Controllers;
+
+public class HelloWorldController : Controller
+{
+    static private List<DogViewModel> dogs = new List<DogViewModel>();
+
+    public IActionResult Index()
+    {
+        return View(dogs);
+    }
+
+    public IActionResult Create()
+    {
+        var dogVm = new DogViewModel();
+        return View(dogVm);
+    }
+
+
+    public IActionResult CreateDog(DogViewModel dogViewModel)
+    {
+        //return View("Index");
+        dogs.Add(dogViewModel);
+        return RedirectToAction(nameof(Index));
+    }
+    public string Hello()
+    {
+        return "Who's there?";
+    }
+}
---
+++ DotNet/FirstMVCApp/README.txt
@@ -0,0 +1,6 @@
+Learning from this video
+https://youtu.be/7d2UMAIgOLQ
+from 0:00 to 1:07.
+
+Subjects:
+Controllers/Views/Layouts/Models/Project Creation.
